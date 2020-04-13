143d8bf5624cec82ff07dc85b7aac8abe6029244
ajanthan <ajanthan.kt@gmail.com>
2020-04-13 17:47:51 +1000

    Remove secret

--- Lyrics.Recorder/Services/ConfigurationService.cs
+++ Lyrics.Recorder/Services/ConfigurationService.cs
@@ -16,11 +16,7 @@ namespace Lyric.Recorder.API.Services
         {
             _Config = Config;
 
-            Configuration.ConnectionString = @"mongodb://muscian:Wc49ajIaLWPAs2GGwNXjp6zgGFqAZn5Mrsi6xvcOSIvRaS70BfF4YJQuM1Xy53gi00n8Tl6H800f5rVozzIR4Q==@muscian.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@muscian@";
-
-            //Configuration.ConnectionString = @"mongodb://contact:rKkAMlY8pHlQ3ROcgIwuzNCJzW1iJEbteOkPTz0I6T0yXtPIdnISuLS60WIRhujod3C9pYkc3jMmh2K5qhzXBQ==@contact.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@contact@;retrywrites = false";
-
-           // _Config.GetConnectionString("DBConnection");
+            _Config.GetConnectionString("DBConnection");
         }
      
     }
